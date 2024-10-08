de488c85cfcda9e365b399ab1ac96f6287868f8a
Miguel Rico <mricofernandez@safareyes.es>
2024-10-08 17:01:50 +0200

    🔥 Remove files to move it

--- src/main/java/com/example/restaurante/models/Mesa.java
+++ src/main/java/com/example/restaurante/models/Mesa.java
@@ -3,7 +3,6 @@ package com.example.restaurante.models;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -15,7 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 @EqualsAndHashCode
 
-public class Mesas {
+public class Mesa {
     @Id
     @GeneratedValue
     @Column(name = "id")
