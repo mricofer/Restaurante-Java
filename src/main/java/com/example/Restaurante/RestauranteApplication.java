7926fcee834c7a12cdd102f67c6d4a05ce7d0536
Miguel Rico <mricofernandez@safareyes.es>
2024-10-16 18:57:04 +0200

    🐛 Fix database connection

--- src/main/java/com/example/Restaurante/RestauranteApplication.java
+++ src/main/java/com/example/Restaurante/RestauranteApplication.java
@@ -1,4 +1,4 @@
-package com.example.restaurante;
+package com.example.Restaurante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
